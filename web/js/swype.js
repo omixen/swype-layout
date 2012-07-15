2ce3ee0e432b53c8cdeeae9fe12bbb1bcc31f349
Tommy Setiawan <omixen@gmail.com>
2012-07-15 19:21:22 -0400

    initially draggable

---
+++ web/js/swype.js
@@ -0,0 +1,48 @@
+(function($) {
+    var dragging = false;
+    var windowWidth = $(window).width();
+    var slideThreshold = 100;
+    var current = 0;
+    var lastBodyPosition = 0;
+    var lastMousePosition = 0;
+    //init
+    $(document).ready(function() {
+
+    });
+    //dragging of swype-body within swype-container
+    $("#swyper").mousedown(function(event){
+        if(!dragging)
+        {
+            dragging = true;
+            lastMousePosition = event.pageX;
+            lastBodyPosition = $(this).position().left;
+        }
+    });
+    $("#swyper").mouseup(function(event){
+        if(dragging)
+        {
+            dragging = false;
+            //slide into place
+            var posX = event.pageX;
+            if(posX < slideThreshold)
+            {
+                //slide to next if exist, if not slide back
+            }else if(posX > windowWidth-slideThreshold)
+            {
+                //slide to previous if exist, if not slide back
+            }else
+            {
+                //slide back
+            }
+        }
+    });
+    $("#swyper").mousemove(function(event){
+        if(dragging)
+        {
+            //drag the swype-body
+            var distance = event.pageX - lastMousePosition;
+            $(this).css("left", (lastBodyPosition+distance));s
+        }
+    });
+    //slide into place when
+})(jQuery);
