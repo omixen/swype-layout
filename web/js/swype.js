31587f45d8719277fc2f01d9907f0498806d6054
Tommy Setiawan <omixen@gmail.com>
2012-07-15 21:07:33 -0400

    using on event

--- web/js/swype.js
+++ web/js/swype.js
@@ -5,53 +5,51 @@
     var current = 0;
     var lastBodyPosition = 0;
     var lastMousePosition = 0;
-    //init
+    //no need to use pageinit here, we are not using any ajax request
     $(document).ready(function() {
-
-    });
-    //dragging of swype-body within swype-container
-    if(Modernizr.touch)
-    {
-        $("#swyper").live('touchstart', function(event){
-            if(!dragging)
-            {
-                dragging = true;
-                lastMousePosition = event.pageX;
-                lastBodyPosition = $(this).position().left;
-            }
-        });
-        $("#swyper").live('touchstop', function(event){
-            if(dragging)
-            {
-                dragging = false;
-                //slide into place
-                var posX = event.pageX;
-                if(posX < slideThreshold)
+        //dragging of swype-body within swype-container
+        if(Modernizr.touch)
+        {
+            $(document).on('#swyper', 'touchstart', function(event){
+                if(!dragging)
                 {
-                    //slide to next if exist, if not slide back
-                }else if(posX > windowWidth-slideThreshold)
-                {
-                    //slide to previous if exist, if not slide back
-                }else
-                {
-                    //slide back
+                    dragging = true;
+                    lastMousePosition = event.pageX;
+                    lastBodyPosition = $(this).position().left;
                 }
-            }
-        });
-        $("#swyper").live('touchmove', function(event){
-            if(dragging)
-            {
-                //drag the swype-body
-                var distance = event.pageX - lastMousePosition;
-                $(this).css("left", (lastBodyPosition+distance));
-            }
-            event.preventDefault();
-        });
-    }else
-    {
-        //show next and prev button
+            });
+            $(document).on('#swyper', 'touchstop', function(event){
+                if(dragging)
+                {
+                    dragging = false;
+                    //slide into place
+                    var posX = event.pageX;
+                    if(posX < slideThreshold)
+                    {
+                        //slide to next if exist, if not slide back
+                    }else if(posX > windowWidth-slideThreshold)
+                    {
+                        //slide to previous if exist, if not slide back
+                    }else
+                    {
+                        //slide back
+                    }
+                }
+            });
+            $(document).on('#swyper', 'touchmove', function(event){
+                if(dragging)
+                {
+                    //drag the swype-body
+                    var distance = event.pageX - lastMousePosition;
+                    $(this).css("left", (lastBodyPosition+distance));
+                }
+                event.preventDefault();
+            });
+        }else
+        {
+            //show next and prev button
 
-        //bind click event
-    }
-
+            //bind click event
+        }
+    });
 })(jQuery);
